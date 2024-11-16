36041cc8317ecf725ccfe85f702cd9c869cf3098
Fehd <mohamed-fehd.soufi.1@ens.etsmtl.ca>
2024-11-16 13:17:33 -0500

    Python script that needs to be run first to download the files from loghub, and create folders

--- download_logfiles.py
+++ download_logfiles.py
@@ -10,15 +10,19 @@ os.makedirs("input", exist_ok=True)
 os.makedirs("HDFS_results", exist_ok=True)
 os.makedirs("BGL_results", exist_ok=True)
 
-# List of files to download with their URLs
+# List of files to download with their URLs and extraction folders
 files_to_download = [
-    {"url": "https://zenodo.org/records/8196385/files/HDFS_v1.zip?download=1", "name": "HDFS_v1.zip"},
-    {"url": "https://zenodo.org/records/8196385/files/BGL.zip?download=1", "name": "BGL.zip"}
+    {"url": "https://zenodo.org/records/8196385/files/HDFS_v1.zip?download=1", "name": "HDFS_v1.zip", "extract_to": "input/HDFS_v1"},
+    {"url": "https://zenodo.org/records/8196385/files/BGL.zip?download=1", "name": "BGL.zip", "extract_to": "input/BGL"}
 ]
 
 for file_info in files_to_download:
     # Define the output path for the current file
     output_path = os.path.join("input", file_info["name"])
+    extract_to = file_info["extract_to"]
+
+    # Create the extraction folder if it doesn't exist
+    os.makedirs(extract_to, exist_ok=True)
 
     # Stream the file and display a progress bar
     response = requests.get(file_info["url"], stream=True)
@@ -36,10 +40,10 @@ for file_info in files_to_download:
                 bar.update(len(chunk))
         print(f"File downloaded to {output_path}")
 
-        # Unzip the downloaded file
+        # Unzip the downloaded file to the specific folder
         with zipfile.ZipFile(output_path, "r") as zip_ref:
-            zip_ref.extractall("input")
-        print(f"File unzipped to 'input' folder")
+            zip_ref.extractall(extract_to)
+        print(f"File unzipped to '{extract_to}'")
 
     else:
         print(f"Failed to download {file_info['name']}. HTTP status code: {response.status_code}")
